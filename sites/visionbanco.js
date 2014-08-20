b5ce0bfd0d1f56df78a1bd7e0eb67685c91a7549
Kenny Meyer <knny.myer@gmail.com>
2014-08-20 13:11:58 -0400

    Fix entering password in Vision Banco interface

--- sites/visionbanco.js
+++ sites/visionbanco.js
@@ -1,5 +1,6 @@
 var password_field = document.getElementById("textfield");
 password_field.removeAttribute("onkeyup");
+password_field.removeAttribute("readonly");
 
 var virtual_keyboard = document.getElementById("a_0").parentElement;
 virtual_keyboard.remove();
